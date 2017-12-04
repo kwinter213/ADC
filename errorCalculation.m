335f9752b542bcac20efad8d0ff99c535970b1b2
Lauren <lauren.pudvan@gmail.com>
2017-12-04 16:11:57 -0500

    Updated to add some error messages

--- errorCalculation.m
+++ errorCalculation.m
@@ -1,5 +1,9 @@
 function y = errorCalculation(expected,actual)
     errorCounter = 0;
+    
+    if length(expected) > length(actual)
+        error('expected is longer than actual')
+    end
     for i = 1:length(expected)
         if expected(i) ~= actual(i)
             errorCounter = errorCounter + 1;
