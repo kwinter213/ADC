67d0c494b6217702a557494c3112b48d573ef08d
March S <marchsaper@gmail.com>
2017-12-05 15:11:49 -0500

    Add files via upload

---
+++ hamming_decode.m
@@ -0,0 +1,31 @@
+function decoded = hamming_decode(received)
+% Function decodes Hamming encoded message using 
+% It takes an encoded row vector of length 7 and returns the decoded
+% vector. Can correct up to 1 bit error.
+
+if length(received) != 7
+    disp("input vector wrong length");
+    break
+end
+
+wrongIndex = 0;
+corrected = received;
+
+% parity check matrix in order of increasing binary numbers
+H = [0 0 0 1 1 1 1;
+     0 1 1 0 0 1 1;
+     1 0 1 0 1 0 1];
+
+check = mod(received*H',2); % if 1 bit error, returns vector which matches column corresponding to incorrect location
+
+wrongBit =bi2de(check);     % convert binary number/column error to index
+    
+if wrongBit > 0
+    corrected(wrongBit) = corrected(wrongBit) + 1;
+    corrected = mod(corrected, 2);  % correct the error (if exists)
+    
+end
+
+decoded = [corrected(3), corrected(5), corrected(6), corrected(7)]; % decoded message, removes parity bits
+
+end
---
+++ hamming_encode.m
@@ -0,0 +1,19 @@
+function encoded = hamming_encode(message)
+% Function generates encoded message using 
+% Hamming coding method. It takes a row vector of length 4.
+% Generator matrix G produces a 7 bit matrix with 3 parity bits.
+
+if length(message) != 4
+    disp("Input vector wrong length");
+    break
+end
+
+G = [0 1 1 1 0 0 0;
+     1 0 1 0 1 0 0;
+     1 1 0 0 0 1 0;
+     1 1 1 0 0 0 1]; 
+
+encoded = (message*G);
+encoded = mod(encoded, 2);
+
+end
