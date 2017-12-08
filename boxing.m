87634d35e6ef5266e18904e4e3bd65f2ff22f168
Lauren <lauren.pudvan@gmail.com>
2017-12-07 21:30:23 -0500

    this might work

--- boxing.m
+++ boxing.m
@@ -3,7 +3,7 @@ temporaryvector = zeros(length(PostHammingFile) * BoxWidth);
 BoxOfOnes = ones(BoxWidth);
 for i = 1:length(PostHammingFile)
     toAdd = BoxOfOnes * i;
-    temporaryvector[(i-1)*BoxWidth +1:(i-1)*BoxWidth+1+BoxWidth] = toAdd;
+    temporaryvector((i-1)*BoxWidth +1:(i-1)*BoxWidth+1+BoxWidth) = toAdd;
 end
 boxedFunction = temporaryvector;
 end
