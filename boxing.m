aa9841a4c691b04dca639258b92b13a21cb3c64c
Lauren <lauren.pudvan@gmail.com>
2017-12-07 22:34:55 -0500

    Update boxing.m
    It Works!!!

--- boxing.m
+++ boxing.m
@@ -1,9 +1,18 @@
 function boxedFunction = boxing(PostHammingFile,BoxWidth)
-temporaryvector = zeros(length(PostHammingFile) * BoxWidth);
-BoxOfOnes = ones(BoxWidth);
-for i = 1:length(PostHammingFile)
-    toAdd = BoxOfOnes * i;
-    temporaryvector((i-1)*BoxWidth +1:(i-1)*BoxWidth+1+BoxWidth) = toAdd;
+% This function takes in the box width and what is to be transmitted. It
+% will make what is to be transmitted into boxes of with values from
+% PostHammingFile and make it into boxes of width BoxWidth.
+
+% Creates a vector of 0s that will be the final length
+temporaryvector = zeros(length(PostHammingFile) * BoxWidth,1); 
+% Creates vector of 1s length of a box
+BoxOfOnes = ones(BoxWidth,1);
+
+for i = 1:length(PostHammingFile) % Goes through values to transmit 
+    toAdd = BoxOfOnes * PostHammingFile(i); % making the box to add
+    % Overwriting the section of 0s with the box to add
+    temporaryvector((i-1)*BoxWidth +1:(i-1)*BoxWidth+BoxWidth) = toAdd; 
 end
+% Making the result
 boxedFunction = temporaryvector;
 end
