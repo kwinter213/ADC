3a2aa2827c32c0e76e3d9fc52a2d4d60ad357734
March S <marchsaper@gmail.com>
2017-12-09 11:54:07 -0500

    Update trimming.m

--- trimming.m
+++ trimming.m
@@ -1,27 +1,18 @@
-function y = trimming(recievedFileToBeTrimmed, starter)
+function y = trimming(recievedFileToBeTrimmed, starter, ender)
 %     This is a function to trimm the received data file to just the
-%     message.
+%     message. Starter and ender must have an even length. 
 
-
-    autocorrelation = (xcorr(real(recievedFileToBeTrimmed),real(recievedFileToBeTrimmed)));
-%     This will find the amount of noisy points in the file. So before the
-%     desired section + after. Will be used to help error proof
-    for i = 1:length(autocorrelation)
-        if abs(autocorrelation(i)) > 1e-3
-            amountOfNoiseyPoints = i;
-            break
-        end
-    end
+   
+%     Finding the ammount to cut. 
+    [corrWithStarter, starterLag] = (xcorr(recievedFileToBeTrimmed,starter));
+    [~,I] = max(abs(corrWithStarter));
+    lagStart = starterLag(I) - length(starter)/2;   % Not the actual start, must account for header offset
     
-%     Finding the length of the recieved message
-    lengthRecievedMessage = (length(autocorrelation)/2) - amountOfNoiseyPoints;
-    
-%     Finding the ammount to cut. Val is not used but needed to get index
-    corrWithStarter = (xcorr(real(recievedFileToBeTrimmed),real(starter)));
-    [ValMaxCorrStarter,IdxMaxCorrStarter] = max(corrWithStarter);
-    cutAmout = IdxMaxCorrStarter - (length(starter)/2);
-    
+%    Finding the ending point. 
+    [corrWithEnder, enderLag] = (xcorr(recievedFileToBeTrimmed,ender));
+    [~,J] = max(abs(corrWithEnder));
+    lagEnd = enderLag(J) - length(ender)/2;   % Not the actual start, must account for header offset
     
 %     result is trimmed file where only the front is trimmed off
-    y = recievedFileToBeTrimmed(cutAmout:length(recievedFileToBeTrimmed)); 
+    y = recievedFileToBeTrimmed(lagStart:lagEnd); 
 end
