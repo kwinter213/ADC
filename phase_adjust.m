479a64a8e20cb3eec1861e60d08f4e9cdc89b8c9
kwinter213 <Kimberly.Winter@students.olin.edu>
2017-12-11 23:41:51 -0500

    Update phase_adjust.m

--- phase_adjust.m
+++ phase_adjust.m
@@ -1,8 +1,8 @@
-function corrected = phase_adjust(received)
-% This function assumes Raleigh flat fading channel model 
-% and corrects for constant phase offsets 
+function [corrected,headoff] = phase_adjust(received,rHead)
+%This function assumes Raleigh flat fading channel model 
+%and corrects for constant phase offsets 
 
-corrected = zeros(length(received-1)); 
+%corrected = zeros(length(received-1)); 
 
 Fourth = received.^4;   % ^4 makes 1 + 1j term constant
 FFTfourth = fftshift(abs(fft(Fourth))); % looking for freq offset
@@ -13,12 +13,36 @@ plot(ws, FFTfourth)
 angfreqOff = (ws(I)/4); % find corresponding angular frequency of offset
 
 for k = 1:length(received)-1
-   corrected(k) = exp(-angfreqOff*k*1i).*received(k);   % adjust for phase offset
+   corrected(k) = received(k)./exp(angfreqOff*k*j);   % adjust for phase offset
+end
+for k =length(rHead)-18:length(rHead)-1
+    headoff(k)= exp(-angfreqOff*k*1j).*rHead(k); %adjust for phase offset
 end
 
 figure;
-hold on
+hold on;
+plot(rHead(length(rHead)-20:length(rHead)-1), 'r.');
+plot(headoff,'b.');
+counter=0;
+for k=1:length(headoff)-1
+    a=real(headoff(k));
+    b=imag(headoff(k));
+    counter=counter+atand(b/a);
+end
+
+offsettot=counter/length(headoff) %taking the average of these points is now our phase offset avg.
+move=315-offsettot
+
+for k = 1:length(corrected)-1
+   newcorrected(k) = corrected(k)/exp((-move)*1j);   % adjust for phase offset
+end
+newcorrected
+figure;
+hold on;
 plot(received, 'r.');
 plot(corrected, 'b.');
+plot(newcorrected,'g.');
+
+
 
 end
