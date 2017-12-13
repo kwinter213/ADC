f413778d4d2adade779852dae09a538c4948e3ab
kwinter213 <Kimberly.Winter@students.olin.edu>
2017-12-13 00:50:16 -0500

    Update SendingScript.m

--- SendingScript.m
+++ SendingScript.m
@@ -1,15 +1,15 @@
 Header = norandHead;
 MessageReal = real(Laurens);
 HammedMessageReal = hamming_encode(MessageReal');
-BoxedMessageReal = boxing(HammedMessageReal,5);
+BoxedMessageReal = boxing(MessageReal,5)';
 
 MessageImag = imag(Laurens);
 HammedMessageImag = hamming_encode(MessageImag');
-BoxedMessageImag = boxing(HammedMessageImag,5);
+BoxedMessageImag = boxing(MessageImag,5)';
 
 Message = BoxedMessageReal + BoxedMessageImag*sqrt(-1);
 for i =1:1000
     ones(i)=1;
 end
-ToSend = [ones,zeros(1000,1)', Header, Message', zeros(1000,1)', ones]; % complex vector
+ToSend = [ones,zeros(1000,1)', Header, Message, zeros(1000,1)', ones]; % complex vector
 write_usrp_data_file(ToSend);
