fdd817064c3148a8ae5d61d47431755de16adac8
March S <marchsaper@gmail.com>
2017-12-12 23:32:13 -0500

    Update SendingScript.m

--- SendingScript.m
+++ SendingScript.m
@@ -1,6 +1,13 @@
 Header = norandHead;
-Message = Laurens;
-HammedMessage = hamming_encode(Message);
-BoxedMessage = boxing(HammedMessage,50);
-ToSend = vertcat(Header, BoxedMessage);
+MessageReal = real(Laurens);
+HammedMessageReal = hamming_encode(MessageReal');
+BoxedMessageReal = boxing(HammedMessageReal,5);
+
+MessageImag = imag(Laurens);
+HammedMessageImag = hamming_encode(MessageImag');
+BoxedMessageImag = boxing(HammedMessageImag,5);
+
+Message = BoxedMessageReal + BoxedMessageImag*sqrt(-1);
+
+ToSend = vertcat(Header', Message); % complex vector
 write_usrp_data_file(ToSend);
