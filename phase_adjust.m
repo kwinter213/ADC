4b37eb6d75447bbf471f26bc1abd52abe7ca40bb
kwinter213 <Kimberly.Winter@students.olin.edu>
2017-12-12 22:42:51 -0500

    Update phase_adjust.m

--- phase_adjust.m
+++ phase_adjust.m
@@ -1,40 +1,37 @@
-function [corrected,headoff] = phase_adjust(received,rHead)
+function [newcorrected] = phase_adjust(received,rHead)
 %This function assumes Raleigh flat fading channel model 
 %and corrects for constant phase offsets 
 
 %corrected = zeros(length(received-1)); 
 
 Fourth = received.^4;   % ^4 makes 1 + 1j term constant
-FFTfourth = fftshift(abs(fft(Fourth))); % looking for freq offset
+FFTfourth = fftshift(fft(Fourth)); % looking for freq offset
 ws =(-pi:2*pi/length(Fourth):pi-2*pi/length(Fourth));   % corresponding angular frequency
-figure; 
-plot(ws, FFTfourth)
-[~,I] = max(FFTfourth); % find index of maximum frequency (phase offset)
+
+[P,I] = max(FFTfourth); % find index of maximum frequency (phase offset)
 angfreqOff = (ws(I)/4); % find corresponding angular frequency of offset
+phaseOff=angle(P)/4;
 
 for k = 1:length(received)-1
-   corrected(k) = received(k)./exp(angfreqOff*k*j);   % adjust for phase offset
+   corrected(k) = received(k)./exp(angfreqOff*k*j+phaseOff*j);   % adjust for phase offset
 end
 for k =length(rHead)-18:length(rHead)-1
-    headoff(k)= exp(-angfreqOff*k*1j).*rHead(k); %adjust for phase offset
+    headoff(k)= rHead(k)./exp(-angfreqOff*k*1j); %adjust for phase offset
 end
 
-figure;
-hold on;
-plot(rHead(length(rHead)-20:length(rHead)-1), 'r.');
-plot(headoff,'b.');
 counter=0;
-for k=1:length(headoff)-1
+for k=1:length(headoff)
     counter=counter+angle(headoff(k));
 end
 
 offsettot=counter/length(headoff); %taking the average of these points is now our phase offset avg.
 degrees=offsettot*180/pi;
-move=315-degrees;
+move=145-degrees;
 
-for k = 1:length(corrected)-1
+for k = 1:length(corrected)
    newcorrected(k) = corrected(k)/exp((-move)*1j);   % adjust for phase offset
 end
+
 newcorrected;
 figure;
 hold on;
