26309db1c985121c88f064ece3e5a2b94a83b5fd
March S <marchsaper@gmail.com>
2017-12-15 06:53:36 -0500

    Update receiving_script.m

--- receiving_script.m
+++ receiving_script.m
@@ -1,20 +1,32 @@
-messageLength = 1040+240;
-boxWidth = 5;
-load('norandHead.mat')
-load('recievedLaurensNoRandUSRPRead.mat')
-load('Laurens.mat')
-recievedUSRPread = read_usrp_data_file('recievedLaurensNoRandUSRPRead.dat');
-[receHead,reciMess] = trimmingsketch(recievedUSRPread,norandHead,messageLength);
-[adjustedMess] = phase_adjust(reciMess,receHead);
+Header = norandHead;
+SentMessage = Message;
+messageLength = length(Header)+length(SentMessage);
+boxWidth = 50;
+
+receivedUSRPread = read_usrp_data_file();
+plot(real(receivedUSRPread))
+
+[receHead,reciMess] = trimming(receivedUSRPread,Header,messageLength);
+
+[adjustedMess] = phase_adjust(reciMess,receHead, Header);    %phase adjust the unboxed function. Need known header
+
 RealSign = sign(real(adjustedMess));
 ImagSign = sign(imag(adjustedMess));
-Signs = RealSign + ImagSign*sqrt(-1);
+
+unboxedMessageReal = unboxing(real(Message), boxWidth);
+unboxedMessageImag = unboxing(imag(Message), boxWidth);
+
 unboxedReal = unboxing(RealSign, boxWidth);
 unboxedImag = unboxing(ImagSign, boxWidth);
+
+%Error calc that takes into account the fact that only half the expected
+%message will actually be sent, usually, by the USRP for unknown reasons.
+%And accounts for weird transmission quirk
+probablyend = ceil(length(unboxedMessageReal)*.55);
+preDecodeError = errorCalculation(vertcat(unboxedMessageReal(1:probablyend-1), unboxedMessageImag(1:probablyend-1)), vertcat(unboxedReal(2:probablyend), unboxedImag(2:probablyend)));
+
 HammingDecodedReal = hamming_decode(unboxedReal);
 HammingDecodedImag = hamming_decode(unboxedImag);
-RealImagDecoded = horzcat(HammingDecodedReal,HammingDecodedImag);
-LaurensReal = real(Laurens);
-LaurensImag = imag(Laurens);
-LaurensRealImag = horzcat(LaurensReal,LaurensImag);
-errorRate = errorCalculation(LaurensRealImag,RealImagDecoded);
+Word1 = bits_to_string(HammingDecodedReal)
+Word2 = bits_to_string(HammingDecodedImag)
+
