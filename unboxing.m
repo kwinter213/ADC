eacbf27d77d3010f8407b097a3e8764f93b51f31
March S <marchsaper@gmail.com>
2017-12-15 06:55:27 -0500

    Update unboxing.m

--- unboxing.m
+++ unboxing.m
@@ -2,12 +2,14 @@ function unboxed = unboxing(received, boxWidth)
 % Function takes boxed input function and box size and outputs 
 % bit string (downsamples)
 
+jumpForward = ceil(boxWidth/2); %trying to sample from middle of box
+
 unboxed = zeros(ceil(length(received)/boxWidth), 1);
 index = 1;
 
-for i = 1:length(received)
+for i = 1:length(received)-jumpForward
     if mod(i, boxWidth) == 0
-            unboxed(index) = received(i);
+            unboxed(index) = received(i+jumpForward);
             index = index + 1;
     end
 end
