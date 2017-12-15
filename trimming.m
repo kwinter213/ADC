3e31308dce1d83b922d685b33bb4190be9c359d7
March S <marchsaper@gmail.com>
2017-12-15 06:54:53 -0500

    Update trimming.m

--- trimming.m
+++ trimming.m
@@ -7,22 +7,29 @@ function [receHead,reciMess] = trimmingsketch(recievedFileToBeTrimmed, starter, 
 [~,I] = max(abs(corrWithStarter));
 lagStart = starterLag(I) + 1;   % Not the actual start, must account for header offset
 disp(lagStart)
-
+figure;
+%plot(starterLag, corrWithStarter);
 
 messageStart = 0;
-for i = lagStart-300:length(recievedFileToBeTrimmed)
-    if abs(real(recievedFileToBeTrimmed(i))) > 1e-3
+for i = lagStart-1000:length(recievedFileToBeTrimmed)
+    if abs(real(recievedFileToBeTrimmed(i))) > .01
         messageStart = i;
         break
     end
 end
 
 if messageStart == lagStart-300
-    error('triiming error, retransmit')
+    error('triming error, retransmit')
 end
     disp(real(recievedFileToBeTrimmed(messageStart)))
     disp(messageStart)
 %     result is trimmed file where only the front is trimmed off
 receHead = recievedFileToBeTrimmed(messageStart:messageStart+length(starter)-1);
 reciMess = recievedFileToBeTrimmed(messageStart+length(starter):messageStart+lengthStartAndMsg-1); 
+
+figure;
+plot(real(recievedFileToBeTrimmed(messageStart-100:end)));
+plot(imag(recievedFileToBeTrimmed(messageStart-100:end)));
+
 end
+
