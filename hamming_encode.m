a0387247178bde0c2cfd8e3b0265d892d9e0c448
March S <marchsaper@gmail.com>
2017-12-15 06:52:21 -0500

    Update hamming_encode.m

--- hamming_encode.m
+++ hamming_encode.m
@@ -1,7 +1,6 @@
 function encodedMessage = hamming_encode(Inputmessage)
 % Function generates encoded message using 
 % Hamming coding method. Message to be encoded. Message must multple of 4.
-Inputmessage=sign(Inputmessage+1);
 encodedMessage = [];
 for i = 1:4:length(Inputmessage)
     
@@ -11,16 +10,17 @@ if length(message) ~= 4
     disp('Input vector wrong length');
 end
 
-G = [0 1 1 1 0 0 0;
-     1 0 1 0 1 0 0;
-     1 1 0 0 0 1 0;
-     1 1 1 0 0 0 1]; 
+G = [1 1 1 0 0 0 0;
+     1 0 0 1 1 0 0;
+     0 1 0 1 0 1 0;
+     1 1 0 1 0 0 1]; 
 
 encoded = (message*G);
 encoded = mod(encoded, 2);
-
+disp(encoded);
 
 encodedMessage = horzcat(encodedMessage, encoded); 
 end
-encodedMessage=sign(encodedMessage-.5);
+
 end
+
